6dad869c403a893ec04b7624fe64a00e3894e11c
Daria Chaiun <dariachiun@gmail.com>
2025-07-15 00:48:59 +0300

    all tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,29 @@
-function checkForSpam(message){
-    const lowerCaseMessage = message.toLowerCase();
-    return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
-}
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+// Об'єкт профілю з методами
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  // Метод повертає інфу про профіль
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+
+  // Метод змінює ім'я користувача
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  // Метод додає годин до playTime
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+};
+
+// Тестові виклики
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
